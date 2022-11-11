507f0d18236cc651b46d15e8cf65a9f084a2a261
night <yseddiq@gmail.com>
2022-11-10 20:05:03 -0500

    added try/catch statement to auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,1 +1,30 @@
 const jwt = require('jsonwebtoken');
+
+const secret = 'mysecret';
+const expiration= '3h';
+
+module.exports ={
+    authMiddleware: function(req, res,next){
+        //allows token to be sent via req.query or headers.
+        let token = req.query.token || req.headers.authorization;
+
+        //[Bearer, "<token value>"]
+        if(req.headers.authorization){
+            token=token
+            .split('')
+            .pop()
+            .trim();
+        }
+        if(!token){
+            return res.status(400).json({ message: 'invalid token'});
+        }
+        try{
+            const { data } = jwt.verify(token, secret, {maxAge: expiration});
+        } catch {
+            console.log('invalid token');
+            return res.status(400).json({ message: 'invalid token'});
+        }
+        //send to next endpoint.
+        next();
+    }
+}
