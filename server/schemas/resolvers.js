3a8ebd2938f369ec8ed7a60ca7306df76f8c9122
night <yseddiq@gmail.com>
2022-11-10 20:24:34 -0500

    added get all user and get single user by username qqueries to resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,6 +4,7 @@ const {signToken}= require('../utils/auth');
 
 const resolvers = {
     Query: {
+      //get logged in user data.
       me: async (parent, args, context)=>{
         if(context.user){
           const userData = await User.findOne({ _id: context.user._id})
@@ -11,6 +12,16 @@ const resolvers = {
           return userData
         }
         throw new AuthenticationError('not logged in')
+      },
+      // get all users.
+      users: async()=>{
+        return User.find()
+          .select('-__v -password')
+      },
+      //get single user by username.
+      user: async (parent, {username})=>{
+        return User.findOne({username})
+          .select('-__v -password');
       }
     }
   };
