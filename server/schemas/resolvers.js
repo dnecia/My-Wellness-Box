56c0748aede92f09c04b05380c88188079a9db25
night <yseddiq@gmail.com>
2022-11-10 20:20:45 -0500

    added me query to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,16 @@
+const {User} = require('../models');
+const {AuthenticationError} = require('apollo-server-express');
+const {signToken}= require('../utils/auth');
+
 const resolvers = {
     Query: {
-      testing: () => {
-        return 'testing';
+      me: async (parent, args, context)=>{
+        if(context.user){
+          const userData = await User.findOne({ _id: context.user._id})
+          .select('-__v -password')
+          return userData
+        }
+        throw new AuthenticationError('not logged in')
       }
     }
   };
