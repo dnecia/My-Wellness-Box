7d91955677ab4058d82a5b5823f2c5dda9cdb10b
Donniecia  Corke <donnieciamc@gmail.com>
2022-11-12 10:28:51 -0500

    updating the login page and app.js folder so that it can connect to the components and pages files.

--- client/src/app.js
+++ client/src/app.js
@@ -1,21 +1,34 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import "./pages/pages.css";
+import React from 'react';
+import { BRowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink,} from '@apollo/client';
+import {setContext} from '@apollo/client/link/context';
+//connecting to the components 
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Login from './components/Login';
+import SingleReview from './components/SingleReview'
 
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" exact element={<Index />}></Route>
-          <Route path="/signup" exact element={<Signup />}></Route>
-          <Route path="/login" exact element={<Login />}></Route>
-        </Routes>
-      </Router>
-    </div>
-  );
-}
+//connecting to the pages 
 
-export default App;
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import Signup from './pages/Signup';
+import SingleReview from './pages/SingleReview';
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token'); 
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+export default App; 
