8cced969b040eeac2b4753d333a867a05d1e250e
mitchellygonzalez <107009737+mitchellygonzalez@users.noreply.github.com>
2022-11-13 14:46:51 -0500

    finished editing profile and single review pages in pages, and commentform and comment list in components

---
+++ client/pages/profile.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { Navigate, useParams } from 'react-router-dom';
+
+import ReviewForm from '../components/ReviewForm';
+import ReviewList from '../components/ReviewList';
+import FriendList from '../components/FriendList';
+
+import { useQuery, useMutation } from '@apollo/client';
+import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import { ADD_FRIEND } from '../utils/mutations';
+import Auth from '../utils/auth';
+
+const Profile = (props) => {
+  const { username: userParam } = useParams();
+
+  const [addFriend] = useMutation(ADD_FRIEND);
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+    variables: { username: userParam },
+  });
+
+  const user = data?.me || data?.user || {};
+
+  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    return <Navigate to="/profile:username" />;
+  }
+
+  if (loading) {
+    return <div>Preparing your R-R profile...</div>;
+  }
+
+  if (!user?.username) {
+    return (
+      <h4>
+        Please sign up or log in to view this page
+      </h4>
+    );
+  }
+
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id },
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <div>
+      <div className="flex-row mb-3">
+        <h2 className="bg-dark text-secondary p-3 display-inline-block">
+          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+        </h2>
+
+        {userParam && (
+          <button className="btn ml-auto" onClick={handleClick}>
+            Add Friend
+          </button>
+        )}
+      </div>
+
+      <div className="flex-row justify-space-between mb-3">
+        <div className="col-12 mb-3 col-lg-8">
+          <ReviewList
+            reviews={user.reviews}
+            title={`${user.username}'s reivews...`}
+          />
+        </div>
+
+        <div className="col-12 col-lg-3 mb-3">
+          <FriendList
+            username={user.username}
+            friendCount={user.friendCount}
+            friends={user.friends}
+          />
+        </div>
+      </div>
+      <div className="mb-3">{!userParam && <ReviewForm />}</div>
+    </div>
+  );
+};
+
+export default Profile;
