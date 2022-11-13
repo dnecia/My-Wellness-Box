c45130b5eabc18d6bc07ecf977d85750ee296d2b
night <yseddiq@gmail.com>
2022-11-13 18:13:54 -0500

    added some useQuery Hooks in Home.js

--- client/pages/Home.js
+++ client/pages/Home.js
@@ -2,3 +2,12 @@ import React from 'react';
 import { useQuery } from '@apollo/client';
 import {QUERY_REVIEWS, QUERY_ME_BASIC} from 'src/utils/queries';
 import Auth from 'src/utils/auth';
+
+const Home = ()=>{
+    //useQuery hook to make request.
+    const{ loading, data}= useQuery(QUERY_REVIEWS)
+    //destructure from useQuery hook's response, and rename it userData.
+    const {data: userData} = useQuery(QUERY_ME_BASIC)
+
+    const loggedIn=Auth.loggedIn();
+}
