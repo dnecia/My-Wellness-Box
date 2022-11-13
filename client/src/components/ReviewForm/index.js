3e008c81b7f0ba73eb37bb4abd2112a1c6d9a4c3
night <yseddiq@gmail.com>
2022-11-13 18:29:02 -0500

    added useState hooks in Home.js, working on useMutation

--- client/src/components/ReviewForm/index.js
+++ client/src/components/ReviewForm/index.js
@@ -2,3 +2,17 @@ import React, {useState} from'react';
 import {useMutation} from '@apollo/client';
 import {ADD_REVIEW} from '../../utils/mutations'
 import { QUERY_REVIEWS, QUERY_ME} from '../../utils/queries'
+
+const ReviewForm=()=>{
+    //sets initial state of text to an empty string.
+    const[reviewText, setText] =useState('');
+    //sets initial state of of characterCount to 0
+    const[characterCount, setCharcterCount] = useState(0);
+
+    const [addReview, {error}] = useMutation(ADD_REVIEW,{
+        
+    });
+
+}
+
+export default ReviewForm
