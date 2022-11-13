685b1ce12325c189a2ae795c49c112f829c9a533
Donniecia  Corke <donnieciamc@gmail.com>
2022-11-13 11:58:37 -0500

    setting up the sign up page and styling

--- server/server.js
+++ server/server.js
@@ -3,14 +3,14 @@ const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
 
 const {typeDefs, resolvers} = require('./schemas');
-//const {authMiddleware} = require('./utils/auth');
+const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  //context: authMiddleware,
+  context: authMiddleware,
 });
 
 const app = express();
@@ -23,9 +23,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-//app.get('*', (req, res) => {
-  //res.sendFile(path.join(__dirname, '../client/build/index.html'));
-//});
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
