dfd2080ff2af231dcfa75a58d98cd42a0870a95c
Donniecia  Corke <donnieciamc@gmail.com>
2022-11-13 13:11:01 -0500

    updating the auth.js file in server side

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,38 +1,41 @@
 const jwt = require('jsonwebtoken');
 
-const secret = 'mysecret';
+const secret = "mysecretsshhhhh";
 const expiration= '3h';
 
-module.exports ={
-    authMiddleware: function(req, res,next){
-        //allows token to be sent via req.query or headers.
-        let token = req.query.token || req.headers.authorization;
-
-        //[Bearer, "<token value>"]
-        if(req.headers.authorization){
-            token=token
-            .split('')
-            .pop()
-            .trim();
-        }
-        if(!token){
-            //if no token is found, throw this error message.
-            return res.status(400).json({ message: 'token does not exist.'});
-        }
-        //verify token and retrieve user data from token.
-        try{
-            const { data } = jwt.verify(token, secret, {maxAge: expiration});
-            req.user=data;
-        } catch {
-            console.log('invalid token');
-            return res.status(400).json({ message: 'invalid token'});
-        }
-        //send to next endpoint.
-        next();
+module.exports = {
+  
+    signToken: function ({ username, email, _id }) {
+      const payload = { username, email, _id };
+  
+      return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
     },
-    signToken: function({username,email,_id}){
-        const payload= {username, email, _id};
-
-        return jwt.sign({ data: payload}, secret, {expiresIn: expiration});
-    }
-}
+  
+    authMiddleware: function ({ req }) {
+      // allows token to be sent via req.body, req.query, or headers
+      let token = req.body.token || req.query.token || req.headers.authorization;
+  
+    
+      if (req.headers.authorization) {
+        token = token.split(" ").pop().trim();
+      }
+  
+      // if no token, return request object as is
+      if (!token) {
+        return req;
+      }
+  
+  
+      try {
+        
+        const { data } = jwt.verify(token, secret, { maxAge: expiration });
+        req.user = data;
+      } catch {
+        console.log("Invalid token");
+      }
+  
+      // return updated request object
+      return req;
+    },
+  };
+  
