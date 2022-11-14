d491896c504338d4192206ab79d9a51455e25251
Donniecia  Corke <donnieciamc@gmail.com>
2022-11-14 18:29:59 -0500

    adding in the local storage to the auth page so that it saves user token

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -23,9 +23,13 @@ class AuthService {
 
     getToken() {
         return localStorage.getItem('id_token');
-        window.location.assign('/');
     }
 
+    login(idToken) {
+        localStorage.setItem('id_token', idToken);
+        window.location.assign('/');
+    }
+        
     logout() {
         localStorage.removeItem('id_token');
         window.location.assign('/')
