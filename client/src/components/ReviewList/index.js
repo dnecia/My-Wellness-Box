c79a24125ae16ce18b7916427e8a8db6089e8682
night <yseddiq@gmail.com>
2022-11-13 19:23:39 -0500

    fixed typo

--- client/src/components/ReviewList/index.js
+++ client/src/components/ReviewList/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const ReactionList=()=>{
+const ReviewList=({reviews})=>{
     return(
         <div>
+            <div>
+                <span>Reviews</span>
+            </div>
+            <div>
+  
+            </div>
         </div>
     )   
 }
