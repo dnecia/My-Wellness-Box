ae6adfdf9d85e29f16ca6c14b039872586ed7caa
Donniecia  Corke <donnieciamc@gmail.com>
2022-11-14 18:06:26 -0500

    updating friendslist to include ability to pick frineds

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="w-100 mt-auto bg-secondary p-4">
       <div className="container">
-        &copy;{new Date().getFullYear()} by 
+        &copy;{new Date().getFullYear()} by Donniecia Mitchell and Younus
       </div>
     </footer>
   );
