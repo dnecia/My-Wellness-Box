d11a8d1f21aacefe8a43626d74115fc3d71f7e83
night <yseddiq@gmail.com>
2022-11-13 19:20:52 -0500

    created ReactionList function skeleton for ReactionList component

--- client/src/components/ReviewList/index.js
+++ client/src/components/ReviewList/index.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
+const ReactionList=()=>{
+    return(
+        <div>
+        </div>
+    )   
+}
+
+export default ReactionList;
