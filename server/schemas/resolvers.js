e488f40538ce90faf5f03fc8bda4b99f1c4a9461
Donniecia  Corke <donnieciamc@gmail.com>
2022-11-14 21:58:37 -0500

    adding in the comment and reviews into the resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,68 +3,93 @@ const {AuthenticationError} = require('apollo-server-express');
 const {signToken}= require('../utils/auth');
 
 const resolvers = {
-    Query: {
-      //get logged in user data.
-      me: async (parent, args, context)=>{
-        if(context.user){
-           const userData = await User.findOne({ _id: context.user._id})
-            .select('-__v -password')
-            .populate("reviews")
-            .populate("friends");
-
-          return userData
-        }
-        throw new AuthenticationError('not logged in')
-      },
-      // get all users.
-      users: async()=>{
-        return User.find()
+  Query: {
+    me: async (parent, args, context)=>{
+      if(context.user){
+        const userData = await User.findOne({ _id: context.user._id})
           .select('-__v -password')
           .populate("reviews")
           .populate("friends");
-      },
-      //get single user by username.
-      user: async (parent, {username})=>{
-        return User.findOne({username})
-          .select('-__v -password')
-          .populate("friends")
-          .populate("thoughts");
-      },
-      reviews: async (parent, { username }) => {
-        const params = username ? { username } : {};
-        return Review.find(params).sort({ createdAt: -1 });
-      },
-      thought: async (parent, { _id }) => {
-        return Review.findOne({ _id });
+
+        return userData
       }
+      throw new AuthenticationError('not logged in')
     },
-    Mutation:{
-      //creates new user.
-      addUser: async (parent, args)=>{
-        const user =await User.create(args);
-        const token= signToken(user);
-
-        return{token,user}
-      },
-      //finds user by email, and checks if password is correct. if both are correct, then login.
-      login: async (parent, {email, password})=>{
-        const user= await User.findOne({email});
-        //if no user found with that email, then throw error.
-        if (!user){
-          throw new AuthenticationError('wrong credentials')
-        }
-
-        const correctPw=await user.isCorrectPassword(password);
-        //if password isn't correct, then throw error.
-        if(!correctPw){
-          throw new AuthenticationError('wrong credentials')
-        }
-
-        const token= signToken(user);
-        return{token,user}
-      }
+    // get all users.
+    users: async()=>{
+      return User.find()
+        .select('-__v -password')
+        .populate("reviews")
+        .populate("friends");
+    },
+    user: async (parent, {username})=>{
+      return User.findOne({username})
+        .select('-__v -password')
+        .populate("friends")
+        .populate("reviews");
+    },
+    reviews: async (parent, { username }) => {
+      const params = username ? { username } : {};
+      return Review.find(params).sort({ createdAt: -1 });
+    },
+    review: async (parent, { _id }) => {
+      return Review.findOne({ _id });
     }
-  };
+  },
+
+  Mutation:{
+    //creates new user.
+    addUser: async (parent, args)=>{
+     const user =await User.create(args);
+     const token= signToken(user);
+
+     return{token,user}
+    },
+    login: async (parent, {email, password})=>{
+      const user= await User.findOne({email});
+      //if no user found with that email, then throw error.
+      if (!user){
+        throw new AuthenticationError('Wrong credentials')
+      }
+
+      const correctPw=await user.isCorrectPassword(password);
+      //if password isn't correct, then throw error.
+      if(!correctPw){
+        throw new AuthenticationError('Wrong credentials')
+      }
+
+      const token= signToken(user);
+      return{token,user}
+    },
+    addReview: async (parent, args, context) => {
+      if (context.user) {
+        const review = await Review.create({ ...args, username: context.user.username });
+
+        await User.findByIdAndUpdate(
+          {_id: context.user._id},
+          { $push: { reviews: review._id } },
+          { new: true }
+        );
+
+        return thought;
+      } 
+
+      throw new AuthenticationError ('Please Log In');
+    },
+    addComment: async (parent, { thoughtId, reactionBody }, context) => {
+      if (context.user) {
+        const updateReview = await Review.findOneAndUpdate(
+          {_id: reviewId },
+          { $push: { comments: { commentBody, username: context.user.username }}},
+          { new: true, runValidators: true }
+        )
+      }
+                
+     }
+
+
+  }
+};
   
   module.exports = resolvers;
 
