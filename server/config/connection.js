438913a81be8eb2f31f7abc788ee622088b46018
Donniecia  Corke <donnieciamc@gmail.com>
2022-11-16 21:12:41 -0500

    adding heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/restaurantreviews',
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/restaurant-reviews',
 {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    //useCreateIndex: true,
-   //useFindAndModify: false
 });
 module.exports = mongoose.connection;
